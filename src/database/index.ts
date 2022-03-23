874c67ae26be23bffa9ed16a0843e3a19516a646
Elias <eliasmdes@gmail.com>
2022-03-23 16:46:54 -0300

    Adicionado teste de integração com supertest

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,15 @@
-import { createConnection } from 'typeorm';
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-(async () => await createConnection())();
+export default async (): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
+
+  return createConnection(
+    Object.assign(defaultOptions, {
+      host: process.env.HOST,
+      database:
+        process.env.NODE_ENV === "test"
+          ? process.env.DATABASE_TEST
+          : defaultOptions.database,
+    })
+  );
+};

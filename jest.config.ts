efd9ccfd5bc6fdd5a466d76d17155ee99fb6c5a6
Elias <eliasmdes@gmail.com>
2022-03-28 10:47:30 -0300

    Exemplo de coverage report

--- jest.config.ts
+++ jest.config.ts
@@ -4,10 +4,19 @@ import { compilerOptions } from "./tsconfig.json";
 export default {
   bail: true,
   clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
   coverageProvider: "v8",
+  collectCoverageFrom: [
+    "**/useCases/**",
+    "**/repositories/**",
+    "!**/node_modules/**",
+    "!**/*DTO.ts",
+    "!**/I*.ts",
+  ],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: "<rootDir>/src/",
   }),
   preset: "ts-jest",
-  testMatch: ["**/*.spec.ts"],
+  //testMatch: ["**/*.spec.ts"],
 };
